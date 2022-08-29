e8cd2a938da868ee6423fb36b972f568e3e459db
James McGuire <james@sadbox.org>
2022-08-28 23:08:56 -0500

    be a little cleaner with images for testing scripts

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -euo pipefail
-docker build -t sprobot .
-docker run -it --mount="type=bind,source=$(grealpath config),target=/config" sprobot $@
+image_sha=$(docker build -q .)
+docker run --rm -it --mount="type=bind,source=$(grealpath config),target=/config" "$image_sha" $@
--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-docker build -t sprobot .
-docker run -it --mount="type=bind,source=$(grealpath config),target=/config" sprobot testing/run-linters.sh
-docker run -it --mount="type=bind,source=$(grealpath config),target=/config" sprobot testing/run-tests.sh
+image_sha=$(docker build -q .)
+docker run -it --mount="type=bind,source=$(grealpath config),target=/config" "$image_sha" testing/run-linters.sh
+docker run --rm -it --mount="type=bind,source=$(grealpath config),target=/config" "$image_sha" testing/run-tests.sh
