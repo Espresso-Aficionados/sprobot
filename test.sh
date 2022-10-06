10a4b6d6f73b6465ceb4b7468386eb4027ab3491
James McGuire <james@sadbox.org>
2022-10-05 22:47:07 -0500

    Update test helper to include volume for mypy caches, speeding up subsequent lint runs

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 docker build --target lint -t sprobot-lint .
-docker run --rm -it --env-file ./config/config.env sprobot-lint
+docker run --rm -it --env-file ./config/config.env -v sprobot-mypy-cache:/code/sprobot/.mypy_cache -v sprobot-web-mypy-cache:/code/sprobot-web/.mypy_cache sprobot-lint
 
 docker build --target test -t sprobot-test .
 docker run --rm -it --env-file ./config/config.env sprobot-test 
