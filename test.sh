d2e822f3ff34d04ec7eaeda5f3d5b0ce3338ba30
James McGuire <james@sadbox.org>
2024-05-14 20:03:51 -0500

    incude pyright file

--- test.sh
+++ test.sh
@@ -5,6 +5,6 @@ docker buildx build --load --target lint --platform linux/arm64,linux/amd64 -t s
 docker buildx build --load --target test --platform linux/arm64,linux/amd64 -t sprobot-test .
 
 for platform in linux/arm64 linux/amd64; do
-    docker run --rm -it  --platform $platform --env-file ./config/config.env sprobot-lint
-    docker run --rm -it  --platform $platform --env-file ./config/config.env sprobot-test 
+    docker run --rm -it  --platform $platform sprobot-lint
+    docker run --rm -it  --platform $platform sprobot-test
 done
